8946f6bb21add77bf37ba956612753decc8f1047
Collin Blosser <43074703+VisualSource@users.noreply.github.com>
2026-02-06 18:04:10 +0000

    use html5lib-tests for tests

--- internal/html_tokenizer/tokenizer_test.go
+++ internal/html_tokenizer/tokenizer_test.go
@@ -1,180 +1,114 @@
 package html_tokenizer
 
 import (
+	"fmt"
 	"io"
-	"slices"
 	"strings"
 	"testing"
 )
 
-func handleCompare(t *testing.T, checks map[string]any, to *Tokenizer) {
-	for i, v := range checks {
+type testCase struct {
+	Description   string   `json:"description"`
+	Input         string   `json:"input"`
+	Output        [][]any  `json:"output"`
+	InitialStates []string `json:"initialStates"`
+	LastStartTag  string   `json:"lastStartTag"`
+	DoubleEscaped bool     `json:"doubleEscaped"`
+	Errors        []struct {
+		Code string
+		line int
+		col  int
+	} `json:"errors"`
+}
 
-		switch i {
-		case "tokens":
-			result := slices.EqualFunc(v.([]Token), to.tokens, func(e1, e2 Token) bool {
-				if target, ok := e1.(TokenCharacter); ok {
-					current, ok := e2.(TokenCharacter)
-					if !ok {
-						return false
-					}
-					return target.Value == current.Value
-				}
+// https://github.com/html5lib/html5lib-tests
 
-				if _, ok := e1.(TokenEOF); ok {
-					_, ok := e2.(TokenEOF)
-					if !ok {
-						return false
-					}
-					return true
-				}
+func TestTokenizer(t *testing.T) {
+	tests := []testCase{}
+	for _, tt := range tests {
 
-				if target, ok := e1.(TokenComment); ok {
-					current, ok := e2.(TokenComment)
-					if !ok {
-						return false
-					}
-					return target.Value == current.Value
-				}
+		if len(tt.InitialStates) != 0 {
+			for _, state := range tt.InitialStates {
+				iState := resolveStateStrToType(state)
 
-				return false
-			})
+				t.Run(fmt.Sprintf("%s: %s", state, tt.Description), func(t *testing.T) {
+					tok := NewTokenizer(strings.NewReader(resolveInputEncoding(tt.Input, tt.DoubleEscaped)))
+					tok.state = iState
 
-			if !result {
-				t.Errorf("failed: expected: '%v' to match '%v'", to.tokens, v)
+					runTest(t, tok, &tt)
+				})
 			}
-		case "state":
-			if to.state != v.(TokenizerState) {
-				t.Errorf("failed: expected: '%v' but got '%v'", v, to.state)
+			continue
+		}
+
+		t.Run(tt.Description, func(t *testing.T) {
+			tok := NewTokenizer(strings.NewReader(resolveInputEncoding(tt.Input, tt.DoubleEscaped)))
+			runTest(t, tok, &tt)
+		})
+	}
+}
+
+func resolveStateStrToType(value string) TokenizerState {
+	return State_Data
+}
+
+func resolveInputEncoding(input string, isDoubleEscaped bool) string {
+	if isDoubleEscaped {
+		return input
+	}
+
+	return input
+}
+
+func runTest(t *testing.T, tok *Tokenizer, tt *testCase) {
+	t.Helper()
+	for {
+		err := tok.Next()
+		if err != nil {
+			if err != io.EOF {
+				t.Fatalf("reader error: %v", err)
 			}
-		case "rstate":
-			if to.rstate != v.(TokenizerState) {
-				t.Errorf("failed: expected: '%v' but got '%v'", v, to.state)
-			}
-		default:
-			t.Errorf("no check setup for '%v'", i)
+			break
 		}
 	}
 
-}
+	if len(tt.Output) != 0 {
 
-func TestTokenizer_state_Data(t *testing.T) {
-	tests := []struct {
-		name string // description of this test case
-		// Named input parameters for receiver constructor.
-		stream  io.RuneReader
-		wantErr bool
-		check   map[string]any
-	}{
-		{
-			name:    "Test Ampersand",
-			stream:  strings.NewReader("&"),
-			wantErr: false,
-			check: map[string]any{
-				"rstate": State_Data,
-				"state":  state_CharacterReference,
-			},
-		},
-		{
-			name:    "Test Less Than",
-			stream:  strings.NewReader("<"),
-			wantErr: false,
-			check: map[string]any{
-				"state": state_TagOpen,
-			},
-		},
-		{
-			name:    "Test EOF",
-			stream:  strings.NewReader(""),
-			wantErr: false,
-			check: map[string]any{
-				"tokens": []Token{NewTokenEOF()},
-			},
-		},
-		{
-			name:    "Test Anything Else",
-			stream:  strings.NewReader("anything"),
-			wantErr: false,
-			check: map[string]any{
-				"tokens": []Token{NewTokenCharacter('a')},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			to := NewTokenizer(tt.stream)
-			gotErr := to.state_Data()
-			if gotErr != nil {
-				if !tt.wantErr {
-					t.Errorf("state_Data() failed: %v", gotErr)
+		for idx, arg := range tt.Output {
+			if idx > len(tok.tokens) {
+				t.Fatalf("token count does not match expecting output")
+			}
+			token := tok.tokens[idx]
+
+			switch arg[0].(string) {
+			case "StartTag":
+
+			case "EndTag":
+				if tag, ok := token.(*TokenEndTag); ok {
+					if arg[1].(string) != tag.name {
+						t.Fatalf("was expecting end tag to have tag name of '%s' not '%v'", arg[1], tag.name)
+					}
+				} else {
+					t.Fatalf("was expecting an end tag but got '%v'", token)
 				}
-				return
-			}
-			if tt.wantErr {
-				t.Fatal("state_Data() succeeded unexpectedly")
-			}
+			case "DOCTYPE":
+			case "Character":
+			case "Comment":
+				if tag, ok := token.(*TokenComment); ok {
+					if arg[1].(string) != tag.Value {
+						t.Fatalf("was expecting comment to have value '%s' not '%s'", arg[1], tag.Value)
+					}
 
-			handleCompare(t, tt.check, to)
-		})
-	}
-}
-
-func TestTokenizer_state_RCData(t *testing.T) {
-	tests := []struct {
-		name string // description of this test case
-		// Named input parameters for receiver constructor.
-		stream  io.RuneReader
-		wantErr bool
-		check   map[string]any
-	}{
-		{
-			name:    "Ampersand",
-			stream:  strings.NewReader("&"),
-			wantErr: false,
-			check: map[string]any{
-				"rstate": State_RCData,
-				"state":  state_CharacterReference,
-			},
-		},
-		{
-			name:    "Less Than Sign",
-			stream:  strings.NewReader("<"),
-			wantErr: false,
-			check: map[string]any{
-				"state": state_RCData_LessThanSign,
-			},
-		},
-		{
-			name:    "Null",
-			stream:  strings.NewReader("\u0000"),
-			wantErr: false,
-			check: map[string]any{
-				"tokens": []Token{NewReplacementToken()},
-			},
-		},
-		{
-			name:   "Anything",
-			stream: strings.NewReader("anything"),
-			check: map[string]any{
-				"tokens": []Token{NewTokenCharacter('a')},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			to := NewTokenizer(tt.stream)
-			gotErr := to.state_RCData()
-			if gotErr != nil {
-				if !tt.wantErr {
-					t.Errorf("state_RCData() failed: %v", gotErr)
+				} else {
+					t.Fatalf("was expecting an comment token but got: '%v'", token)
 				}
-				return
-			}
-			if tt.wantErr {
-				t.Fatal("state_RCData() succeeded unexpectedly")
-			}
 
-			handleCompare(t, tt.check, to)
-		})
+			}
+		}
 	}
+
+	if len(tt.Errors) != 0 {
+		// check errors
+	}
+
 }
