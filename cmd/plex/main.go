c2bd94b5c7a5a06d27d145369c69489938db55bf
Collin Blosser <blosserc@hurco.com>
2026-05-26 10:48:22 -0400

    cleanup entry

--- cmd/plex/main.go
+++ cmd/plex/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
 	"os"
 
 	"github.com/VisualSource/plex/internal/core"
 	"github.com/VisualSource/plex/internal/log"
-	"go.uber.org/automaxprocs/maxprocs"
 )
 
 func main() {
@@ -18,21 +16,8 @@ func main() {
 		log.WithSource(),
 	)
 
-	if err := run(logger); err != nil {
+	if err := core.StartPlex(logger); err != nil {
 		logger.ErrorContext(context.Background(), "an error occurred", slog.String("error", err.Error()))
 		os.Exit(1)
 	}
-}
-
-func run(logger *slog.Logger) error {
-	ctx := context.Background()
-
-	_, err := maxprocs.Set(maxprocs.Logger(func(s string, i ...interface{}) {
-		logger.DebugContext(ctx, fmt.Sprintf(s, i...))
-	}))
-	if err != nil {
-		return fmt.Errorf("setting max procs: %w", err)
-	}
-
-	return core.StartPlex(logger)
 }
